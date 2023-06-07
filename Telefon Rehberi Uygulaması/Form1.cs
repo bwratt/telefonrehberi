2cfc26b2adc5edc566850c896fb18380b51b3908
Berat Kaplan <kaplanberat1306@gmail.com>
2023-06-07 16:40:35 +0300

    listeleme

--- Telefon Rehberi Uygulaması/Form1.cs
+++ Telefon Rehberi Uygulaması/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace Telefon_Rehberi_Uygulaması
 {
@@ -15,6 +16,34 @@ namespace Telefon_Rehberi_Uygulaması
         public Form1()
         {
             InitializeComponent();
+        }
+
+        OleDbConnection bag = new OleDbConnection("Provider=Microsoft.Jet.OleDb.4.0;Data Source=telefonrehberi.mdb");
+        OleDbCommand komut = new OleDbCommand();
+
+        DataTable veritablosu = new DataTable();
+
+        public void listele()
+        {
+            try
+            {
+                veritablosu.Clear();
+                OleDbDataAdapter siringa = new OleDbDataAdapter("select * from Tablo1", bag);
+                siringa.Fill(veritablosu);
+                dataGridView1.DataSource = veritablosu;
+
+
+            }
+            catch (Exception hata)
+            {
+                MessageBox.Show(hata.ToString());
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listele();
         }
     }
 }
